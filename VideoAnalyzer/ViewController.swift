c2f90e51a70bfc3f2ef831fcdbcdc9be32b7d59a
Nguyễn Đức Thọ <nguyen.duc.tho@framgia.com>
2018-11-04 13:32:40 +0700

    Initial Commit

---
+++ VideoAnalyzer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoAnalyzer
+//
+//  Created by Nguyễn Đức Thọ on 11/4/18.
+//  Copyright © 2018 Nguyễn Đức Thọ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
